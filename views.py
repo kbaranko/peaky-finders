877f1fa2e69ae54bfa69a6a32ba26e7b5b6b8328
Kyle Baranko <KCBARANKO@GMAIL.COM>
2020-01-06 15:31:12 -0500

    Delete views.py

--- views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-main = Blueprint('main', __name__) #give it name, pass in name of module
-
-#add routes to main object 
-@main.route('/')
-def index():
-    return render_template('home.html')
-
