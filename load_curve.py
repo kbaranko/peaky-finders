b9ab8bab7c26a7366591799ddef62dd68fefbc6c
Kyle Baranko <kcbaranko@gmail.com>
2020-01-30 15:29:09 -0500

    adding new functions, historical load curve

--- load_curve.py
+++ load_curve.py
@@ -6,23 +6,34 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 import dash_gif_component as Gif
 from load_functions import * 
+from forecast_functions import *
 from assets import *
 
 
 
 df = final_forecast(pd.datetime.today().strftime('%Y-%m-%d %H')) 
+df_1 = previous_2days_load()
 
 plot_forecast = go.Scatter(x=list(df.index),
                             y=list(df['Predicted Load']),
                             name="Projected",
-                            line=dict(color="#e76aeb")
+                            line=dict(color="#e76aeb", width=4, dash='dot')
+)
+
+plot_historical = go.Scatter(x=list(df_1.index),
+                            y=list(df_1['load_MW']),
+                            name="Historical",
+                            line=dict(color="#43d9de")
 )
 
 data = [plot_forecast]
+data_1 = [plot_historical]
 
 layout = dict(title="Projected Load Curve", showlegend=True)
+layout_1 = dict(title="7-Day Historical Load Curve", showlegend=True)
 
 fig = dict(data=data, layout=layout)
+fig_1 = dict(data=data_1, layout=layout_1)
 
 app = dash.Dash(__name__)
 
@@ -49,45 +60,15 @@ app.layout = html.Div([
         ]),
     html.Div([
             html.Div([
-                dcc.Graph(id='Load Curve',
-                    figure=fig),
+                dcc.Graph(id='Past Load Curve',
+                    figure=fig_1),
         ], className='six columns'), 
         html.Div([
-            dcc.Graph(id='Load wave',
+            dcc.Graph(id='Projected Load Curve',
                     figure=fig),
         ], className='six columns'),
     ], className='row'),
     ])
-
-    # html.Div([
-    #     html.Div([
-            
-    #     Gif.GifPlayer(gif='assets/giphy.gif', still='assets/giphy.gif'),
-    #     ], className="six columns"),
-    #     ], className='row'),
-    #     html.Div([
-    #         html.H5(id='drop-down-title', children='Select your ISO'),
-    #         html.Div(
-    #             dcc.Dropdown(
-    #                 options=[
-    #                     {'label': 'NYISO', 'value': 'NYISO'},
-    #                     {'label': 'PJM', 'value': 'PJM'},
-    #                     {'label': 'CAISO', 'value': 'CAISO'},
-    #                     ],
-    #                     id='select-iso'),
-    #         ),
-    #     ]),
-    #     html.Div([
-    #         html.Div([
-    #             dcc.Graph(id='Load Curve',
-    #                 figure=fig),
-    #     ], className='six columns'), 
-    #     html.Div([
-    #         dcc.Graph(id='Load wave',
-    #                 figure=fig),
-    #     ], className='six columns'),
-    # ], className='row'),
-    # ])
 
 
 #if the file name assignmed is main, then we'll actually run our server 
@@ -95,8 +76,3 @@ if __name__ == '__main__':
     app.run_server(debug=True)
 
     
-
-# app.css.append_css({
-#     "external_url": "http://codepen.io/chriddyp/pen/bWLwgP.css"
-# })
-
