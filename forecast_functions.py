7133707c7091b6fcfad959c4d6972535ea40d059
Kyle Baranko <kcbaranko@gmail.com>
2020-02-02 16:59:18 -0500

    working on functions

--- forecast_functions.py
+++ forecast_functions.py
@@ -469,6 +469,14 @@ def get_7day_forecast():
     holiday_dummies = pd.get_dummies(result['holiday'], prefix='holiday', drop_first=True)
     result = result.drop(['weekday', 'hour', 'holiday'], axis=1)
     result = pd.concat([result, day_dummies, hour_dummies, holiday_dummies], axis=1)
+    result.columns = ['cloudcover', 'first seasonal difference', 'humidity', 'load (t-24)',
+       'prev-day-hour-MA', 'prev-day-hour-Std', 'temperature', 'uvindex',
+       'day_Monday', 'day_Saturday', 'day_Sunday', 'day_Thursday',
+       'day_Tuesday', 'day_Wednesday', 'hour_1.0', 'hour_2.0', 'hour_3.0',
+       'hour_4.0', 'hour_5.0', 'hour_6.0', 'hour_7.0', 'hour_8.0', 'hour_9.0',
+       'hour_10.0', 'hour_11.0', 'hour_12.0', 'hour_13.0', 'hour_14.0',
+       'hour_15.0', 'hour_16.0', 'hour_17.0', 'hour_18.0', 'hour_19.0',
+       'hour_20.0', 'hour_21.0', 'hour_22.0', 'hour_23.0', 'holiday_1.0']
     ## load model
     xgb_model_loaded = pickle.load(open('xg_boost_load_modelV3.pkl', "rb"))
     master_df = pd.read_csv('training.csv')
