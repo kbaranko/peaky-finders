3700cd543be4e7680b5ec9ea9746517eab0735a3
Kyle Baranko <kcbaranko@gmail.com>
2020-02-13 23:25:56 -0500

    PEP8

--- peaky_packages/functions/load_functions.py
+++ peaky_packages/functions/load_functions.py
@@ -1,13 +1,9 @@
-#import necessary packages 
-from pyiso import client_factory
+#import necessary packages
 import pandas as pd
-import config
-import json
-import numpy as np
 import datetime as dt
 import calendar
-import holidays
 from datetime import date, timedelta
+from pyiso import client_factory
 
 #formats datetime string to prepare for joins
 def format_datetime(row):
@@ -17,22 +13,22 @@ def format_datetime(row):
     row['timestamp'] = datetime_string
     return row
 
-#gets load info for nyiso  
+#gets load info for nyiso
 def previous_7days_load():
     nyiso = client_factory('NYISO', timeout_seconds=60)
     begin = (dt.datetime.today() - timedelta(7)).strftime('%Y-%m-%d %H')
     end = pd.datetime.today().strftime('%Y-%m-%d %H')
-    df = nyiso.get_load(latest=False, yesterday=False, start_at=begin, end_at=end)
-    df = pd.DataFrame(df)
-    df = df[['load_MW', 'timestamp']]
-    df = df.set_index('timestamp')
-    df.index = df.index.tz_convert('US/Eastern')
-    df = df.resample('H').max()
-    df = df.reset_index()
-    df = df.apply(format_datetime, axis=1)
-    df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M')
-    df = df.set_index('timestamp')
-    return df
+    DF = nyiso.get_load(latest=False, yesterday=False, start_at=begin, end_at=end)
+    DF = pd.DataFrame(DF)
+    DF = DF[['load_MW', 'timestamp']]
+    DF = DF.set_index('timestamp')
+    DF.index = DF.index.tz_convert('US/Eastern')
+    DF = DF.resample('H').max()
+    DF = DF.reset_index()
+    DF = DF.apply(format_datetime, axis=1)
+    DF['timestamp'] = pd.to_datetime(DF['timestamp'], format='%Y-%m-%d %H:%M')
+    DF = DF.set_index('timestamp')
+    return DF
 
 if __name__ == '__main__':
     print('load functions imported')
