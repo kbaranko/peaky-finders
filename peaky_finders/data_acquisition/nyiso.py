bc6c1816d7a3ec9ad068b39181021fb6d7ae34e0
Kyle Baranko <kyle@kevalaanalytics.com>
2021-01-10 21:03:23 -0700

    fixing weather api call bug

--- peaky_finders/data_acquisition/nyiso.py
+++ peaky_finders/data_acquisition/nyiso.py
@@ -3,14 +3,13 @@ import datetime as dt
 from datetime import timedelta
 import holidays
 import math
+import os
 
 import json
 import numpy as np
 import pandas as pd
 import pickle
 from pyiso import client_factory
-import pyiso
-# from pyiso import client_factory
 import requests
 
 
@@ -18,8 +17,11 @@ LAT = '40.7128'
 LON = '-73.935242'
 BASE_URL = 'https://api.darksky.net/forecast'
 EXCLUDE = 'flags, minutely, daily, alerts'
-# api_key = config.api_key
-# FULL_DARKSKY_URL = f'{BASE_URL}/{API_KEY}/{LAT},{LON}?exclude={EXCLUDE}'
+API_KEY = os.environ['DARKSKY_KEY']
+FULL_DARKSKY_URL = f'{BASE_URL}/{API_KEY}/{LAT},{LON},'
+LOAD_COLS = ['load_MW', 'timestamp']
+EASTERN_TZ = 'US/Eastern'
+
 
 class NYISO:
 
@@ -27,18 +29,41 @@ class NYISO:
         self.start = start_date
         self.end = end_date
         self.holidays = holidays.UnitedStates()
+        self.load = self.get_historical_load()
+        self.model_input = None
 
-
-    def get_temperature(self) -> pd.DataFrame:
-        weather_response = requests.get(FULL_DARKSKY_URL)
-    
-    
     def get_historical_load(self) -> pd.DataFrame:
         nyiso = client_factory('NYISO', timeout_seconds=60)
-        return nyiso.get_load(latest=False, yesterday=False, start_at=self.start, end_at=self.end)
+        load = pd.DataFrame(
+            nyiso.get_load(
+                latest=False,
+                yesterday=False,
+                start_at=self.start,
+                end_at=self.end)
+            )[LOAD_COLS].set_index('timestamp')
+        load.index = load.index.tz_convert(EASTERN_TZ)
+        return load.resample('H').mean()
 
+    def engineer_weather_features(self):
+        temperatures = dict()
+        for date, _ in self.load.iterrows():
+            date_input = date.strftime('%s')
+            full_url = f'{FULL_DARKSKY_URL}{date_input}?exclude={EXCLUDE}'
+            response = requests.get(full_url)
+            if response.status_code == 200:
+                    print(response.status_code)
+            else: 
+                raise ValueError(f'Error getting data from DarkSky API: '
+                                f'Response Code {response.status_code}')
+            info = response.json()
+            current_info = info['currently']
+            current_temperature = current_info['temperature']
+            temperatures[date] = current_temperature
+        import pdb; pdb.set_trace()
+        self.load['temperature'] = self.load.index.map(temperatures)
 
-
+    def engineer_datetime_features(self):
+        pass
 
     @staticmethod
     def findDay(date):
--- peaky_finders/training_pipeline.py
+++ peaky_finders/training_pipeline.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from peaky_finders.data_acquisition.nyiso import NYISO
 
+[['load_MW', 'timestamp']]
 
 class Pipeline:
 
@@ -13,23 +14,27 @@ class Pipeline:
         """
         self.start = start
         self.end = end
-        self.iso = self._set_iso(iso, start, end)
+        self.iso_name = iso
+        self.iso = None
         self.model = model
 
     def phase_one(self):
         """Data acquisition"""
         # nyiso = NYISO('01-01-2020', '01-01-2021')
-        output = pd.DataFrame(self.iso.get_historical_load())
-        import pdb; pdb.set_trace()
+        self.iso = self._set_iso(self.iso_name, self.start, self.end)
 
     def phase_two(self):
         """Feature engineering """
+        self.iso.engineer_weather_features()
+        import pdb; pdb.set_trace()
+        self.iso.engineer_datetime_features()
 
     def phase_three(self):
         """Model training """
 
     def execute(self):
         self.phase_one()
+        self.phase_two()
 
     @staticmethod
     def _set_iso(iso: str, start: str, end: str):
@@ -39,8 +44,7 @@ class Pipeline:
             print(f'Cannot train model for ISO {iso}')
 
 
-# if __name__ == "__main__":
-#     cli()
+if __name__ == "__main__":
+    pipeline = Pipeline(iso='nyiso', model='load_forecast', start='06-01-2020', end='06-02-2020')
+    pipeline.execute()
 
-pipeline = Pipeline(iso='nyiso', model='load_forecast', start='01-01-2020', end='01-03-2020')
-pipeline.execute()
