3ca329ec028ed13e514df6f651555ac0d7435a8a
Kyle Baranko <kyle@kevalaanalytics.com>
2021-01-13 21:04:34 -0700

    training pipeline complete for nyiso

--- peaky_finders/data_acquisition/nyiso.py
+++ peaky_finders/data_acquisition/nyiso.py
@@ -11,6 +11,7 @@ import pandas as pd
 import pickle
 from pyiso import client_factory
 import requests
+from sklearn import preprocessing
 
 
 LAT = '40.7128'
@@ -21,6 +22,10 @@ API_KEY = os.environ['DARKSKY_KEY']
 FULL_DARKSKY_URL = f'{BASE_URL}/{API_KEY}/{LAT},{LON},'
 LOAD_COLS = ['load_MW', 'timestamp']
 EASTERN_TZ = 'US/Eastern'
+
+US_HOLIDAYS = holidays.UnitedStates()
+CATEGORICAL_FEATURES = ['weekday', 'hour_of_day', 'holiday']
+NUMERICAL_FEATURES = ['temperature', 'load (t-24)']
 
 
 class NYISO:
@@ -44,28 +49,56 @@ class NYISO:
         load.index = load.index.tz_convert(EASTERN_TZ)
         return load.resample('H').mean()
 
-    def engineer_weather_features(self):
+    def engineer_features(self):
         temperatures = dict()
+        holiday_bool = dict()
+        weekdays = dict()
         for date, _ in self.load.iterrows():
-            date_input = date.strftime('%s')
-            full_url = f'{FULL_DARKSKY_URL}{date_input}?exclude={EXCLUDE}'
-            response = requests.get(full_url)
-            if response.status_code == 200:
-                    print(response.status_code)
-            else: 
-                raise ValueError(f'Error getting data from DarkSky API: '
-                                f'Response Code {response.status_code}')
-            info = response.json()
-            current_info = info['currently']
-            current_temperature = current_info['temperature']
-            temperatures[date] = current_temperature
-        import pdb; pdb.set_trace()
+            temperatures[date] = self._get_temperature(date)
+            holiday_bool[date] = self._check_for_holiday(date)
+        self.load['weekday'] = self.load.index.dayofweek
+        self.load['hour_of_day'] = self.load.index.hour
         self.load['temperature'] = self.load.index.map(temperatures)
+        self.load['holiday'] = self.load.index.map(holiday_bool)
+        self.load['load (t-24)'] = self.load.load_MW.shift(24)
 
-    def engineer_datetime_features(self):
-        pass
+    def build_model_input(self):
+        self.model_input = self.dummify_categorical_features(self.load.copy())
+        self.model_input = self.standardize_numerical_features(self.model_input)
 
     @staticmethod
-    def findDay(date):
-        day = dt.datetime.strptime(date, '%Y-%m-%d %H').weekday()
-        return calendar.day_name[day]
+    def _get_temperature(date):
+        date_input = date.strftime('%s')
+        full_url = f'{FULL_DARKSKY_URL}{date_input}?exclude={EXCLUDE}'
+        response = requests.get(full_url)
+        if response.status_code == 200:
+            print(response.status_code)
+        else: 
+            raise ValueError(f'Error getting data from DarkSky API: '
+                            f'Response Code {response.status_code}')
+        info = response.json()
+        current_info = info['currently']
+        return current_info['temperature']
+
+    @staticmethod
+    def _check_for_holiday(day):
+        if day in US_HOLIDAYS:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def dummify_categorical_features(load_df: pd.DataFrame):
+        for feature in CATEGORICAL_FEATURES:
+            dummies = pd.get_dummies(load_df[feature], prefix=feature, drop_first=True)
+            load_df = load_df.drop(feature, axis=1)
+            load_df = pd.concat([load_df, dummies], axis=1)
+            load_df = load_df.dropna(axis=0, how='any')
+        return load_df
+
+    @staticmethod
+    def standardize_numerical_features(load_df: pd.DataFrame):
+        z_scaler = preprocessing.StandardScaler()
+        load_df[NUMERICAL_FEATURES] = z_scaler.fit_transform(
+            load_df[NUMERICAL_FEATURES])
+        return load_df
--- peaky_finders/training_pipeline.py
+++ peaky_finders/training_pipeline.py
@@ -1,12 +1,17 @@
+import pickle
+
+import numpy as np
 import pandas as pd
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 from peaky_finders.data_acquisition.nyiso import NYISO
 
-[['load_MW', 'timestamp']]
 
 class Pipeline:
 
-    def __init__(self, iso: str, model: str, start: str, end: str):
+    def __init__(self, iso: str, model: str, start: str, end: str, save_model_input: bool = False):
         """
         Args:
             iso: the iso to forecast ('nyiso', 'ercot', etc.)
@@ -17,24 +22,38 @@ class Pipeline:
         self.iso_name = iso
         self.iso = None
         self.model = model
+        self.save_model_input = save_model_input
 
     def phase_one(self):
         """Data acquisition"""
-        # nyiso = NYISO('01-01-2020', '01-01-2021')
         self.iso = self._set_iso(self.iso_name, self.start, self.end)
 
     def phase_two(self):
-        """Feature engineering """
-        self.iso.engineer_weather_features()
-        import pdb; pdb.set_trace()
-        self.iso.engineer_datetime_features()
+        """Feature engineering + model preparation """
+        self.iso.engineer_features()
+        self.iso.build_model_input()
+        if self.save_model_input:
+            self.iso.model_input.to_csv(f'training_data/{self.iso_name}_{self.start}_{self.end}.csv')
 
     def phase_three(self):
-        """Model training """
+        """Model training & serialization"""
+        y = self.iso.model_input['load_MW']
+        X = self.iso.model_input.drop('load_MW', axis=1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        reg = xgb.XGBRegressor()
+        reg.fit(X_train, y_train)
+        training_preds = reg.predict(X_train)
+        val_preds = reg.predict(X_test)
+        print('Mean Absolute Error:', mean_absolute_error(y_test, val_preds))  
+        print('Root Mean Squared Error:', np.sqrt(mean_squared_error(y_test, val_preds)))
+        if self.save_model:
+            model_name = f'xg_boost_{self.iso.name}_load_model.pkl'
+            pickle.dump(reg, open(file_name, "wb"))
 
     def execute(self):
         self.phase_one()
         self.phase_two()
+        self.phase_three()
 
     @staticmethod
     def _set_iso(iso: str, start: str, end: str):
@@ -45,6 +64,6 @@ class Pipeline:
 
 
 if __name__ == "__main__":
-    pipeline = Pipeline(iso='nyiso', model='load_forecast', start='06-01-2020', end='06-02-2020')
+    pipeline = Pipeline(iso='nyiso', model='load_forecast', start='05-01-2020', end='05-28-2020')
     pipeline.execute()
 
