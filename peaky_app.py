5884178519d3e18fdb6ac1b68cd493db9b54d8ce
Kyle Baranko <kyle@kevalaanalytics.com>
2021-02-21 18:04:00 -0800

    basic ISO page layout

--- peaky_app.py
+++ peaky_app.py
@@ -4,17 +4,23 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 
-from peaky_finders.predictor import predict_all, ISO_LIST
+from peaky_finders.predictor import predict_all, ISO_LIST, get_peak_data
 
 # demo_list = ['nyiso', 'pjm', 'isone']
-demo_list = ['nyiso']
+demo_list = ['PJM']
 
-load = predict_all(demo_list)
+peak_data = get_peak_data(demo_list)
+load, predictions = predict_all(demo_list)
 
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
-app.title = "Peak Load Forecasting App!"
+TEMPLATE = 'plotly_white'
+
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    suppress_callback_exceptions=True
+)
 
 """Homepage"""
 app.layout = html.Div([
@@ -24,20 +30,43 @@ app.layout = html.Div([
 
 index_page = html.Div([
         html.H1(children="Welcome to Peaky Finders"),
-        dcc.Link(
-            html.Button('HOME', id='home-button', className="mr-1"),
-            href='/'),
-        dcc.Link(
-            html.Button('NYISO', id='nyiso-button', className="mr-1"),
-            href='/nyiso'),
-        dcc.Link(
-            html.Button('PJM', id='pjm-button', className="mr-1"),
-            href='/pjm'),
-        dcc.Link(
-            html.Button('ISONE', id='isone-button', className="mr-1"),
-            href='/isone'),
-    html.Br()
+        html.Div([
+            html.H2(children="Select an ISO to get started."),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        dcc.Link(
+                            html.Button('HOME', id='home-button', block=True, color='azure'),
+                            href='/')),
+                    dbc.Col(
+                        dcc.Link(
+                            html.Button('NYISO', id='nyiso-button', className="mr-1", block=True, color='azure'),
+                            href='/nyiso')),
+                            
+                    dbc.Col(
+                        dcc.Link(
+                            html.Button('PJM', id='pjm-button', className="mr-1", block=True, color='azure'),
+                            href='/pjm')),
+                    dbc.Col(
+                        dcc.Link(
+                            html.Button('ISONE', id='isone-button', className="mr-1", block=True, color='azure'),
+                            href='/isone')),
+                    dbc.Col(
+                        dcc.Link(
+                            html.Button('MISO', id='isone-button', className="mr-1", block=True, color='azure'),
+                            href='/miso')),
+                    dbc.Col(
+                        dcc.Link(
+                            html.Button('CAISO', id='isone-button', className="mr-1", block=True, color='azure'),
+                            href='/caiso'))
+                ]
+            )
+        ]
+    )
 ])
+
+
+"""NYISO LAYOUT"""
 
 nyiso_layout = html.Div([
     html.Div(id='nyiso-content'),
@@ -55,38 +84,9 @@ nyiso_layout = html.Div([
         multi = True,
     ),
     dcc.Graph(id='nyiso-graph')
-    # dcc.Graph(
-    #     figure={
-    #         "data": [
-    #             {
-    #                 "x": predictions['nyiso'].index,
-    #                 "y": predictions['nyiso']['predicted_load'],
-    #                 "type": "lines",
-    #             },
-    #         ],
-    #         "layout": {"title": "Forecasted and Predicted Load for NYISO"},
-    #     },
-    # ),
-    # dcc.Graph(
-    #     figure={
-    #         "data": [
-    #             {
-    #                 "x": actual['nyiso'].index,
-    #                 "y": actual['nyiso']['load_MW'],
-    #                 "type": "lines",
-    #             },
-    #         ],
-    #         "layout": {"title": "Avocados Sold"},
-    #     },
-    # ),
-    # # html.Br(),
-    # dcc.Link('Go to Page 2', href='/nyiso'),
-    # html.Br(),
-    # dcc.Link('Go back to home', href='/'),
 ])
 @app.callback(dash.dependencies.Output('nyiso-content', 'children'),
               [dash.dependencies.Input('nyiso-button', 'value')])
-
 
 
 @app.callback(dash.dependencies.Output('nyiso-graph', 'figure'),
@@ -95,17 +95,101 @@ def plot_nyiso_load_(value):
     fig = go.Figure()
     if 'Actual' in value:
         fig.add_trace(go.Scatter(
-            x=load['nyiso'].index,
-            y=load['nyiso']['load_MW'],
+            x=load['NYISO'].index,
+            y=load['NYISO'].values,
             name='Historical Load',
             line=dict(color='maroon', width=3)))
     if 'Predicted' in value:
         fig.add_trace(go.Scatter(
-            x=load['nyiso'].index,
-            y=load['nyiso']['predicted_load'],
+            x=predictions['NYISO'].index,
+            y=predictions['NYISO'].values,
             name = 'Forecasted Load',
-            line=dict(color='aqua', width=3, dash='dash')))
+            line=dict(color='royalblue', width=3, dash='dash')))
     return fig
+
+
+"""PJM LAYOUT"""
+
+pjm_layout = html.Div([
+    html.Div(id='pjm-content'),
+    dcc.Link(
+        html.Button('HOME', id='home-button', className="mr-1"),
+        href='/'),
+    html.H1('PJM'),
+    dcc.Dropdown(
+        id='pjm-dropdown',
+        options=[
+            {'label': 'Actual', 'value': 'Actual'},
+            {'label': 'Predicted', 'value': 'Predicted'}
+        ],
+        value=['Actual', 'Predicted'],
+        multi=True,
+    ),
+    dcc.Graph(id='pjm-graph'),
+    dbc.Row(
+        [
+            dbc.Col(
+                html.Div([
+                    dcc.Graph(figure=px.histogram(
+                        peak_data['PJM'].resample('D').max(),
+                        x=peak_data['PJM'].resample('D').max()['load_MW'],
+                        nbins=75,
+                        marginal="rug",
+                            ).add_vline(x=predictions['PJM'].values.max())
+                    ),
+                ]
+            ), md=3),
+            dbc.Col(
+                html.Div([
+                    dcc.Dropdown(
+                        id='pjm-scatter-dropdown',
+                        options=[
+                            {'label': 'Day of Week', 'value': 'weekday'},
+                            {'label': 'Season', 'value': 'season'}
+                            ],
+                        value='season',
+                        multi=False,
+                    ),
+                    dcc.Graph(id='pjm-scatter')
+                ]
+            ), md=3),
+        ]
+    )
+])
+@app.callback(dash.dependencies.Output('pjm-content', 'children'),
+              [dash.dependencies.Input('pjm-button', 'value')])
+
+@app.callback(dash.dependencies.Output('pjm-graph', 'figure'),
+             [dash.dependencies.Input('pjm-dropdown', 'value')])
+def plot_pjm_load_(value):
+    fig = go.Figure()
+    if 'Actual' in value:
+        fig.add_trace(go.Scatter(
+            x=load['PJM'].index,
+            y=load['PJM'].values,
+            name='Historical Load',
+            line=dict(color='maroon', width=3)))
+    if 'Predicted' in value:
+        fig.add_trace(go.Scatter(
+            x=predictions['PJM'].index,
+            y=predictions['PJM'].values,
+            name = 'Forecasted Load',
+            line=dict(color='royalblue', width=3, dash='dash')))
+    return fig
+
+@app.callback(dash.dependencies.Output("pjm-scatter", "figure"), 
+    [dash.dependencies.Input("pjm-scatter-dropdown", "value")])
+def update_scatter_plot(value):
+    fig = px.scatter(
+        peak_data['PJM'].resample('D').max(),
+        x="load_MW",
+        y="temperature", 
+        color=value
+    )
+    return fig
+
+
+
 
 # Update the index
 @app.callback(dash.dependencies.Output('page-content', 'children'),
@@ -113,11 +197,11 @@ def plot_nyiso_load_(value):
 def display_page(pathname):
     if pathname == '/nyiso':
         return nyiso_layout
+    elif pathname == '/pjm':
+        return pjm_layout
     else:
         return index_page
 
 
-
-
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
--- peaky_finders/predictor.py
+++ peaky_finders/predictor.py
@@ -5,15 +5,26 @@ import pickle
 from typing import Dict, Tuple
 
 import pandas as pd
+import pytz, datetime
 from timezonefinderL import TimezoneFinder
 
 from peaky_finders.data_acquisition.train_model import (
-    LoadCollector, GEO_COORDS, CATEGORICAL_FEATURES)
+    LoadCollector, GEO_COORDS, CATEGORICAL_FEATURES, MONTH_TO_SEASON)
 from peaky_finders.training_pipeline import MODEL_OUTPUT_DIR
 
 
 ISO_LIST = ['NYISO', 'ISONE', 'CAISO', 'PJM', 'MISO']
 
+PEAK_DATA = {
+    'NYISO': 'NYISO_01-01-2018_01-01-2020.csv',
+    'PJM': 'PJM_01-01-2019_07-28-2020.csv',
+}
+
+PEAK_DATA_PATH = os.path.join(
+    os.path.dirname(__file__), 'training_data')
+
+
+tz_finder = TimezoneFinder()
 
 class Predictor:
 
@@ -56,7 +67,7 @@ class Predictor:
             model_input = pd.concat([model_input, dummies], axis=1)
         return model_input
 
-    def predict_load(self, model_input: pd.DataFrame) -> pd.DataFrame:
+    def predict_load(self, model_input: pd.DataFrame) -> Tuple[pd.Series, pd.Series]:
         model_path = os.path.join(MODEL_OUTPUT_DIR, (f'xg_boost_{self.iso_name}_load_model.pkl'))
         xgb = pickle.load(open(model_path, "rb"))
         if 'holiday_True' not in model_input.columns:
@@ -64,15 +75,30 @@ class Predictor:
         X = model_input.drop('load_MW', axis=1).astype(float).dropna()
         predictions = xgb.predict(X)
         X['predicted_load'] = predictions
-
-        return pd.concat([model_input['load_MW'], X['predicted_load'].drop_duplicates(keep='first')], axis=1)
+        return model_input['load_MW'].drop_duplicates(), X['predicted_load']
 
 
 def predict_all(iso_list: list) -> Tuple[Dict[str, pd.DataFrame]]:
-    load = {}
+    historical_load = {}
+    predicted_load = {}
     for iso in iso_list:
-        predictor = Predictor('PJM')
+        predictor = Predictor(iso)
         model_input = predictor.prepare_predictions()
-        load_df = predictor.predict_load(model_input)
-        load[iso] = load_df
-    return load
+        load, predictions = predictor.predict_load(model_input)
+        historical_load[iso] = load
+        predicted_load[iso] = predictions
+    return historical_load, predicted_load
+
+def get_peak_data(iso_list: list) -> Tuple[Dict[str, pd.DataFrame]]:
+    peak_data = {}
+    for iso in iso_list:
+        iso_data = pd.read_csv(os.path.join(PEAK_DATA_PATH, PEAK_DATA[iso]), parse_dates=['timestamp'])
+        iso_data['timestamp'] = iso_data['timestamp'].apply(lambda x: x.astimezone(pytz.utc))
+        tz_name = tz_finder.timezone_at(lng=float(GEO_COORDS[iso]['lon']), lat=float(GEO_COORDS[iso]['lat']))
+        iso_data.index = pd.DatetimeIndex(iso_data['timestamp'])
+        iso_data.index = iso_data.index.tz_convert(tz_name)
+        basics = iso_data[['load_MW', 'temperature']]
+        basics['weekday'] = basics.index.day_name()
+        basics['season'] = basics.index.month.map(MONTH_TO_SEASON)
+        peak_data[iso] = basics
+    return peak_data
