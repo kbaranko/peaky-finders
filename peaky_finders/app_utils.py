9e27e694b8f8b3a1343b370d7b769dfd0c3f03bb
Kyle Baranko <kyle@kevalaanalytics.com>
2021-07-18 10:53:53 -0400

    utils cleanup

--- peaky_finders/app_utils.py
+++ peaky_finders/app_utils.py
@@ -12,7 +12,6 @@ from peaky_finders.data_acquisition.train_model import GEO_COORDS
 PEAK_DATA_PATH = os.path.join(
     os.path.dirname(__file__), 'historical_peaks')
 
-
 tz_finder = TimezoneFinder()
 
 
@@ -21,10 +20,15 @@ def get_forecasts(iso_list: List[str]):
     historical_load = {}
     temperature = {}
     for iso in iso_list:
-        path = f'https://raw.githubusercontent.com/kbaranko/peaky-finders/master/peaky_finders/forecasts/{iso}_forecasts.csv'
+        path = f'{c.FORECASTS_PATH}/{iso}_forecasts.csv'
         iso_data = pd.read_csv(path, parse_dates=['timestamp'])
-        iso_data['timestamp'] = iso_data['timestamp'].apply(lambda x: x.astimezone(pytz.utc))
-        tz_name = tz_finder.timezone_at(lng=float(GEO_COORDS[iso]['lon']), lat=float(GEO_COORDS[iso]['lat']))
+        iso_data['timestamp'] = iso_data['timestamp'].apply(
+            lambda x: x.astimezone(pytz.utc)
+        )
+        tz_name = tz_finder.timezone_at(
+            lng=float(GEO_COORDS[iso]['lon']),
+            lat=float(GEO_COORDS[iso]['lat'])
+        )
         iso_data.index = pd.DatetimeIndex(iso_data['timestamp'])
         iso_data.index = iso_data.index.tz_convert(tz_name)
         historical_load[iso] = iso_data['load_MW']
@@ -33,21 +37,31 @@ def get_forecasts(iso_list: List[str]):
     return predictions, historical_load, temperature
 
 
-def create_load_duration(peak_data: Dict[str, pd.DataFrame]) -> Dict[str, pd.Series]:
+def create_load_duration(
+    peak_data: Dict[str, pd.DataFrame]
+) -> Dict[str, pd.Series]:
     load_duration_curves = {}
     for iso in c.ISO_LIST:
-        load_duration_curves[iso] = pd.Series(peak_data[iso]['load_MW'].values) \
-            .sort_values(ascending=False)
+        load_duration_curves[iso] = pd.Series(
+            peak_data[iso]['load_MW'].values
+        ).sort_values(ascending=False)
     return load_duration_curves
 
 
-def get_peak_data(iso_list: list) -> Tuple[Dict[str, pd.DataFrame]]:
+def get_peak_data(iso_list: list) -> Dict[str, pd.DataFrame]:
     peak_data = {}
     for iso in iso_list:
-        path = 'https://raw.githubusercontent.com/kbaranko/peaky-finders/master/peaky_finders/historical_peaks'
-        iso_data = pd.read_csv(f'{path}/{iso}_historical_peaks.csv', parse_dates=['timestamp'])
-        iso_data['timestamp'] = iso_data['timestamp'].apply(lambda x: x.astimezone(pytz.utc))
-        tz_name = tz_finder.timezone_at(lng=float(GEO_COORDS[iso]['lon']), lat=float(GEO_COORDS[iso]['lat']))
+        iso_data = pd.read_csv(
+            f'{c.PEAKS_PATH}/{iso}_historical_peaks.csv',
+            parse_dates=['timestamp']
+        )
+        iso_data['timestamp'] = iso_data['timestamp'].apply(
+            lambda x: x.astimezone(pytz.utc)
+        )
+        tz_name = tz_finder.timezone_at(
+            lng=float(GEO_COORDS[iso]['lon']),
+            lat=float(GEO_COORDS[iso]['lat'])
+        )
         iso_data.index = pd.DatetimeIndex(iso_data['timestamp'])
         iso_data.index = iso_data.index.tz_convert(tz_name)
         peak_data[iso] = iso_data
